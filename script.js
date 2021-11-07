33da29a442817b85b9e80b4d57e7267b889e7f9f
claudinei messias <claudineimessias@hotmail.com>
2021-11-07 18:55:53 -0300

    update

--- script.js
+++ script.js
@@ -42,6 +42,12 @@ const transactions = [
         description:"internet", 
         amount: -20000,       // amount= quantia
         date:"23/01/2021"
+    },
+    {
+        id:4, 
+        description:"App", 
+        amount: 200000,       // amount= quantia
+        date:"23/01/2021"
     }
 ]
 
@@ -59,22 +65,28 @@ const Transaction={
 }
 
 // 1 Substituir os dados do HTML com os dados do JS
-const DOM = {
+const DOM = { // 3 metodo vai adicionar
+    transactionsContainer:document.querySelector('#data-table tbody'),
+
+
     addTransaction(transactions,index){  // 2  método vai criar o elemento html 'tr' que vai receber
                                         // o primeiro método e incorporar os elementos html 'tds"
                                         // recebendo "transaction como parametro"
 
-        console.log(transactions)
+       /*  console.log(transactions) */
         const tr = document = document.createElement('tr');
         tr.innerHTML = DOM.innerHTMLTransaction(transactions);
-
-        console.log(tr.innerHTML)
+        /* console.log(tr.innerHTML); */
+        DOM.transactionsContainer.appendChild(tr);
     },
     innerHTMLTransaction(transactions){ //  1 método vai criar "tds"  e vai ter como parametro
                                         // o  objeto "transactions e passar as suas propriedades"
+
+        const CssClass = transactions.amount > 0 ? "income":"expense" //5 caso quantia seja maior que 0 recebe a classe 'income'
+        const amount= Utils.formatCurrency(transactions.amount); // 6
         const html = `
         <td class="description">${transactions.description}</td> 
-        <td class="expense">${transactions.amount}</td>
+        <td class="${CssClass}">${transactions.amount}</td>
         <td class="date">${transactions.date}</td>
         <td><img src="./assets/minus.svg" alt="Remover transação"></td>
         
@@ -82,7 +94,18 @@ const DOM = {
         return html
     }
 }
-DOM.addTransaction(transactions[2]);
+
+const Utils = { // 6
+    formatCurrency(value){
+        const signal = Number(value) < 0 ? "-" : "+"  ; 
+        console.log(value);
+        
+    }
+}
+
+transactions.forEach(function(transaction){ //4  Para cada transação em transactions 
+    DOM.addTransaction(transaction)
+})
 
 
 
