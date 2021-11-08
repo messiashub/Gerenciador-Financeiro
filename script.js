ffb8827a7e6bbe2dbaf7a2bb27f03ac3ac49e090
claudinei messias <claudineimessias@hotmail.com>
2021-11-08 18:25:21 -0300

    update

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let $cancelar = document.getElementById('cancelar');
 let $modal = document.querySelector('.modal-overlay')
 
 
-const modal={
+const modal={ 
     /*funcão para abrir o modal Adiconar  a class active*/
     open : $novaTrans.addEventListener('click',()=>{
     $modal.classList.add("active");
@@ -24,40 +24,55 @@ const modal={
 
 
 // Transações
-const transactions = [
-    {                  /* objeto */
-        id:1, 
-        description:"luz", 
-        amount: -50001,       // amount= quantia
-        date:"23/01/2021"
-    },      
-    {
-        id:2, 
-        description:"Criação de website", 
-        amount: 500000,       // amount= quantia
-        date:"23/01/2021"
-    },
-    {
-        id:3, 
-        description:"internet", 
-        amount: -20013,       // amount= quantia
-        date:"23/01/2021"
-    },
-    {
-        id:4, 
-        description:"App", 
-        amount: 200000,       // amount= quantia
-        date:"23/01/2021"
-    }
-]
+
 
 
 const Transaction={//7.1
+    all: [
+        {                  /* objeto */
+           /*  id:1,  */
+            description:"luz", 
+            amount: -50001,       // amount= quantia
+            date:"23/01/2021"
+        },      
+        {
+           /*  id:2,  */
+            description:"Criação de website", 
+            amount: 500000,       // amount= quantia
+            date:"23/01/2021"
+        },
+        {
+           /*  id:3, */ 
+            description:"internet", 
+            amount: -20013,       // amount= quantia
+            date:"23/01/2021"
+        },
+        {
+           /*  id:4,  */
+            description:"App", 
+            amount: 200000,       // amount= quantia
+            date:"23/01/2021"
+        }
+    ],
+    
+    add(transaction){
+        Transaction.all.push(transaction);
+
+        App.reload()
+
+       /*  console.log(Transaction.all) */
+    },
+
+    remove(index){  //11 pegar o index do array
+        Transaction.all.splice(index,1);
+        App.reload()
+
+    },
     income(){
         let income = 0;
         //pegar todas as transações
         // para cada transação,
-        transactions.forEach((transaction)=>{
+        Transaction.all.forEach((transaction)=>{
             //se ela for maior que zero
             if(transaction.amount > 0){
                 // somar a uma variável e retornar a variavel
@@ -71,7 +86,7 @@ const Transaction={//7.1
          let expense = 0;
         //pegar todas as transações
         // para cada transação,
-        transactions.forEach((transaction)=>{
+        Transaction.all.forEach((transaction)=>{
             //se ela for menor que zero
             if(transaction.amount < 0){
                 // somar a uma variável e retornar a variavel
@@ -129,6 +144,10 @@ const DOM = { // 3 metodo vai adicionar
             .getElementById("totalDisplay")
             .innerHTML = Utils.formatCurrency( Transaction.total()) // Utils 6
 
+    },
+    clearTransactions(){ // 9  para  limpar depois de atualizar
+        DOM.transactionsContainer.innerHTML="";
+
     }
 }
 
@@ -149,14 +168,92 @@ const Utils = { // 6 formatando
     }
 }
 
-transactions.forEach(function(transaction){ //4  Para cada transação em transactions 
+const Form = { 
+    description:document.querySelector("input#description"),// 10 pegando os valores dos campos
+    amount:document.querySelector("input#amount"),// 10
+    date:document.querySelector("input#date"),//10
+
+    getValues(){//10
+        return {
+            description:Form.description.value,
+            amount:Form.amount.value,
+            date:Form.date.value
+        }
+    },
+  
+    validadeFields(){ // 10
+        const {description,amount,date} = Form.getValues();
+
+        if(description.trim() === "" || amount.trim()=== "" || date.trim()=== ""){
+           throw new Error("Por favor preencha todos os campos.");
+        }
+        console.log(Form.getValues())
+    },
+    
+    submit(event){ // 10
+        /* console.log(event) */
+        event.preventDefault();
+         // 1 verificar se todas as informações forem preenchidas
+        
+        try{
+            Form.validadeFields();
+            // 2 formatar os dados para salvar 
+            /* Form.formatData() */
+            // salvar 
+            // apagar os dados do formulario 
+            // fechar Modal
+            // Atualizar a aplicação
+        }catch(error){
+            alert(error.message)
+
+        }
+        
+    }
+}
+
+const App ={ //8
+    init(){
+        Transaction.all.forEach(function(transaction){ //8  Para cada transação em transactions 
+            DOM.addTransaction(transaction)
+        })
+        
+        DOM.updateBalance()
+        
+       /*  Transaction.add({
+            id:39,
+            description:"Alô",
+            amount:200,  
+            date:"08/11/2021"
+        }) */
+        
+
+    },
+    reload(){  //9 função para recarregar a pagina
+        DOM.clearTransactions() // 9 chamando a função para limpar
+        App.init()
+    }
+}
+
+
+App.init();
+/* Transaction.remove(0)
+ */
+
+
+
+/* transactions.forEach(function(transaction){ //4  Para cada transação em transactions 
     DOM.addTransaction(transaction)
 })
 
 DOM.updateBalance()
-
-
-
+*/
+/* Transaction.add({
+   id:39, 
+    description:"Alô",
+    amount:200,  
+    date:"08/11/2021"
+})
+  */
 
 
 
